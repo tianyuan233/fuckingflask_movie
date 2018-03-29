26ce42a7dd96c37be61681e1c6826d279057a58a
tianyuan233 <1406282384@qq.com>
2018-03-29 22:56:39 +0800

    front

--- app/home/views.py
+++ app/home/views.py
@@ -1,14 +1,48 @@
-from . import home
 from flask import render_template, redirect, url_for
+
+from . import home
+
 
 @home.route("/")
 def index():
     return render_template('home/index.html')
 
+
 @home.route("/login/")
 def login():
     return render_template('home/login.html')
 
+
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
+
+
+@home.route("/register/")
+def register():
+    return render_template('home/register.html')
+
+
+@home.route("/user/")
+def user():
+    return render_template('home/user.html')
+
+
+@home.route("/chpwd/")
+def chpwd():
+    return render_template('home/chpwd.html')
+
+
+@home.route("/comments/")
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template('home/loginlog.html')
+
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template('home/moviecol.html')
