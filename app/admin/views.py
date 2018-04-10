e0d949f2ec2167c88b4e5a32f67de7ee6c10221a
tianyuan233 <1406282384@qq.com>
2018-04-10 22:59:33 +0800

    user list

--- app/admin/views.py
+++ app/admin/views.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 
 from app import db, app
 from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
-from app.models import Admin, Tag, Movie, Preview
+from app.models import Admin, Tag, Movie, Preview, User
 from . import admin
 
 
@@ -313,16 +313,33 @@ def preview_edit(id=None):
     return render_template("admin/preview_edit.html", form=form, preview=preview)
 
 
-@admin.route("/user/list/")
+# 会员列表
+@admin.route("/user/list/<int:page>", methods=['GET'])
 @admin_login_req
-def user_list():
-    return render_template("admin/user_list.html")
+def user_list(page):
+    if page is None:
+        page = 1
+    page_data = User.query.order_by(
+        User.addtime.desc()
+    ).paginate(page=page, per_page=5)
+    return render_template("admin/user_list.html", page_data=page_data)
 
 
-@admin.route("/user/view/")
+@admin.route("/user/view/<int:id>", methods=['GET'])
 @admin_login_req
-def user_view():
-    return render_template("admin/user_view.html")
+def user_view(id=None):
+    user = User.query.get_or_404(int(id))
+    return render_template("admin/user_view.html", user=user)
+
+
+@admin.route("/user/del/<int:id>", methods=['GET'])
+@admin_login_req
+def user_del(id=None):
+    user = User.query.get_or_404(int(id))
+    db.session.delete(user)
+    db.session.commit()
+    flash("删除用户成功", "ok")
+    return redirect(url_for("admin.user_list",page=1))
 
 
 @admin.route("/comment/list/")
