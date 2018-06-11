3b6b66375f39e298fb6d46ce5060faa2dee25e30
tianyuan233 <1406282384@qq.com>
2018-06-11 10:52:59 +0800

    创建数据库迁移

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager
 import os
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost:3306/movie"
@@ -11,6 +13,12 @@ app.config["FC_DIR"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
 app.debug = True
 
 db = SQLAlchemy(app)
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db', MigrateCommand)
+
 
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
